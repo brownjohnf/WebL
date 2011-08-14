66d289b2fdc0bff8603faa2c8a3b495cccc488bd
Dennis de Reus <dennisdereus@gmail.com>
2011-08-14 22:13:48 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webl::Application.config.secret_token = '58013865047360a5f9924b14574258ac1ad243c1962e404ed5501c3adac8cd88f9ca5cf63fce6c9e475edee383025c5d86e6fa1d5abfcd21bfb147db2d4fcd9d'
