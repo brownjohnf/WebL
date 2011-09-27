f49e332ad07400aa7afde210ff352dfb93d6237c
Dennis de Reus <dennisdereus@gmail.com>
2011-09-27 21:01:40 +0200

    tokeninput now working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :content, 
 									:publication_date, :published, :comments_disabled,
-									:tags_attributes, :tags
+									:tags_attributes, :tag_tokens
 
 	validates	:title,				:presence => :true,
 													:length => {:within => 3..60}
@@ -20,5 +20,17 @@ class Post < ActiveRecord::Base
 	
 	def to_param
 		"#{id}-#{title.parameterize}"
+	end
+	
+	def tag_tokens
+		@tag_tokens = []
+		self.tags.each do |tag|
+			@tag_tokens << {:id => tag.id, :name => tag.name}
+		end
+		@tag_tokens
+	end
+
+	def tag_tokens=(ids)
+		self.tag_ids = ids.split(",")
 	end
 end 
